48121ebbb9a8b3b18d953f018cbc751e51e12303
Andrew Gozillon <Andrew.Gozillon@amd.com>
2023-08-17 20:17:47 -0500

    remove unneccessary dump

--- mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
+++ mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
@@ -1420,8 +1420,6 @@ mlir::translateModuleToLLVMIR(Operation *module, llvm::LLVMContext &llvmContext,
   if (failed(translator.convertOperation(*module, llvmBuilder)))
     return nullptr;
 
-  // translator.llvmModule->dump();
-
   if (llvm::verifyModule(*translator.llvmModule, &llvm::errs()))
     return nullptr;
 
